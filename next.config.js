1d8279f72778c80bc0d8df340590e2f822f84be7
BeaReis <pcreis.beatriz@gmail.com>
2022-10-12 21:37:21 -0300

    First commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
+
+const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack(config) {
+    config.module.rules.forEach((rule) => {
+      const { oneOf } = rule;
+      if (oneOf) {
+        oneOf.forEach((one) => {
+          if (!`${one.issuer?.and}`.includes('_app')) return;
+          one.issuer.and = [path.resolve(__dirname)];
+        });
+      }
+    })
+    return config;
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { createGlobalStyle } from "styled-components";
 
-function MyApp({ Component, pageProps }: AppProps) {
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none;
+  }
+`;
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
 
