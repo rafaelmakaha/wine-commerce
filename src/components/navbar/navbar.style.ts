14991bfc2056a5d655d4bc9b84bd56578c4132ba
BeaReis <pcreis.beatriz@gmail.com>
2022-10-19 21:12:47 -0300

    Completed navbar styling

--- src/components/navbar/navbar.style.ts
+++ src/components/navbar/navbar.style.ts
@@ -2,35 +2,55 @@ import { StaticImageData } from "next/image";
 import styled from "styled-components";
 import colors from "../../../public/colors.json";
 
+interface Props {
+  type: string;
+}
+
 export const Nav = styled.nav`
   position: sticky;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 88px;
+  height: 58px;
   background-color: ${colors.neutrals.white};
   padding: 15px 150px;
 `;
 
 export const NavItem = styled.a`
   background-color: transparent;
-  font-family: "Roboto Flex", sans-serif;
+  font-family: "Titillium Web", sans-serif;
   font-size: 18px;
   font-weight: 400;
+  cursor: pointer;
 `;
 
 export const NavItemWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 48px;
+  margin: 0 215px 0 80px;
+`;
 
-`
-
-export const NavIcon = styled.img`
-    object-fit: contain;
-    background-color: transparent;
-    height: 56px;
-    width: 56px;
-`
+export const NavIcon = styled.img<Props>`
+  object-fit: contain;
+  cursor: pointer;
+  ${(props) => {
+    if (props.type === "icon") {
+      return `
+        height: 56px;
+        width: 56px;
+        `;
+    } else {
+      return `
+        height: 28px;
+        width: 100px;
+        `;
+    }
+  }}
+`;
 
 export const NavIconWrapper = styled.div`
-
-
-`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 42px;
+`;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,25 +1,36 @@
-import { Nav, NavIcon, NavItem } from "./navbar.style"
+import {
+  Nav,
+  NavIcon,
+  NavIconWrapper,
+  NavItem,
+  NavItemWrapper,
+} from "./navbar.style";
 
 const Navbar: React.FC = () => {
-    const cart = "/cart.png";
-    const busca = "/busca.png";
-    const conta = "/conta.png";
-    
-    return(
-        <>
-            <Nav>
-                <NavItem>WINE</NavItem>
-                <NavItem>Clube</NavItem>
-                <NavItem>Loja</NavItem>
-                <NavItem>Produtores</NavItem>
-                <NavItem>Ofertas</NavItem>
-                <NavItem>Eventos</NavItem>
-                <NavIcon src={busca} />
-                <NavIcon src={conta} />
-                <NavIcon src={cart} />
-            </Nav>
-        </>
-    )
-}
+  const cart = "/cart.png";
+  const busca = "/busca.png";
+  const conta = "/conta.png";
+  const wine = "/wine.png";
 
-export default Navbar
+  return (
+    <>
+      <Nav>
+        <NavIcon src={wine} type="logo"></NavIcon>
+        <NavItemWrapper>
+          <NavItem>Clube</NavItem>
+          <NavItem>Loja</NavItem>
+          <NavItem>Produtores</NavItem>
+          <NavItem>Ofertas</NavItem>
+          <NavItem>Eventos</NavItem>
+        </NavItemWrapper>
+        <NavIconWrapper>
+          <NavIcon src={busca} type="icon" />
+          <NavIcon src={conta} type="icon" />
+          <NavIcon src={cart} type="icon" />
+        </NavIconWrapper>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
